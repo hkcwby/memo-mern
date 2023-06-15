d4f5fd852d7a696ef5cd4448d7a5559faf0f0902
hkcwby <alexanderstevenson@me.com>
2023-06-14 23:13:58 -0500

    creating addtional routes for delete and post/patch options

--- backend/memo-routes.js
+++ backend/memo-routes.js
@@ -13,6 +13,24 @@ router.route("/fetchdata").get((req, res) => {
     .catch((err) => res.status(400).json("Error: " + err));
 });
 
+router.route("/:id").delete((req, res) => {
+  // MemoData.findByIdAndDelete(req.params.id)
+  MemoData.findOneAndDelete({ id: req.query.id })
+    .then(console.log("successful delete"))
+    .catch((err) => res.status(400).json("Error: " + err));
+});
+
+router.route("/update/:id").post((req, res) => {
+  MemoData.find(req.params.id)
+    .then((memo) => {
+      memo.title = req.body.title;
+      memo.detail = req.body.detail;
+      memo.save();
+    })
+    .then(console.log("successful update"))
+    .catch((err) => res.status(400).json("Error: " + err));
+});
+
 router.route("/add").post((req, res) => {
   const id = req.body.id;
   const title = req.body.title;
